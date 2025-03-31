c3c8e3bd6cf9a4eb6ff662bceb31e057d57542e6
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-31 14:43:00 +0300

    Created functions for generating, saving and getting secret_key

--- server.py
+++ server.py
@@ -13,3 +13,13 @@ class User:
         self.connection_string = ('DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=Test;Trusted_Connection=yes')
         self.conn = pyodbc.connect(self.connection_string)
         self.cursor = self.conn.cursor()
+
+    def generate_and_store_secret_key(self):
+        self.cursor.execute('UPDATE Users SET SecretKey = ? WHERE Login = ?', (secret_key, self.username))
+        self.conn.commit()
+        return secret_key
+
+    def get_secret_key(self):
+        self.cursor.execute('SELECT SecretKey FROM Users WHERE Login = ?', (self.username,))
+        result = self.cursor.fetchone()
+        return result[0] if result else None
